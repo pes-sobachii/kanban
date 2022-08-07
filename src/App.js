3990d4a82211834347b3ea3f5a3238844bf85f2c
Alex Solohub <le03grande@gmail.com>
2022-08-07 17:08:07 +0300

    hhhhh

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import {TodoBody} from "./Components/Body";
 import {Provider} from "react-redux";
 import store from "./Redux/react-redux";
-import {HashRouter} from 'react-router-dom';
 
 function App() {
     return (
         <div className="App">
-            <HashRouter basename={process.env.PUBLIC_URL}>
-                <Provider store={store}>
-                    <TodoBody/>
-                </Provider>
-            </HashRouter>
+            <Provider store={store}>
+                <TodoBody/>
+            </Provider>
         </div>
     );
 }
